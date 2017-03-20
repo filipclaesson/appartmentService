31561909a935576e9286b08015673f82fce40b5b
filip <filipclaesson@hotmail.com>
2017-03-20 22:51:14 +0100

    adding package

--- postgres_caller.js
+++ postgres_caller.js
@@ -2,7 +2,7 @@ var pg = require('pg');
 
 // -- postgres pass
 var pass;
-s = require('fs')
+fs = require('fs')
 fs.readFile('/home/pi/node_apps/postgres_pass.txt', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
